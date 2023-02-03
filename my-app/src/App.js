c78d105d1e942a38cc0ff951f603787a95de2ccc
FIRST_NAME LAST_NAME <snieska.juozas@gmail.com>
2023-02-03 23:49:39 +0200

    created new function example to create full design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './app.css';
+import Design from './components/design';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="main-age">
+      <Design />
     </div>
   );
 }
---
+++ my-app/src/components/design.js
@@ -0,0 +1,20 @@
+import './design.css'
+
+function Design() {
+  return (
+      <div className="main">
+        <h2>ReizhTech HomeWork Assignment</h2>
+        {/* <div className='filters'>
+          <div>
+          <input>First</input>          
+          <input>Second</input>          
+          </div>
+          <div>
+            <button>Press</button>
+          </div>
+        </div> */}
+      </div>
+  );
+}
+
+export default Design
